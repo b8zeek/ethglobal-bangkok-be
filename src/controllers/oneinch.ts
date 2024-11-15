5d7d2e7602c6ff16f0407c30ef29c0602bf9a354
Mirko Basic <bejzik8@gmail.com>
2024-11-16 04:40:23 +0700

    Added token details route.

--- src/controllers/oneinch.ts
+++ src/controllers/oneinch.ts
@@ -38,3 +38,24 @@ export const getProfitAndLoss: RequestHandler = async (req, res, next) => {
         next(err)
     }
 }
+
+export const getTokenDetails: RequestHandler = async (req, res, next) => {
+    const { walletAddress, chainId } = req.body
+
+    try {
+        const { data } = await axios.get(
+            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/details?addresses=${walletAddress}&chain_id=${chainId}&timerange=1day`,
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+                }
+            }
+        )
+
+        console.log(data)
+
+        res.status(200).json({ response: data })
+    } catch (err) {
+        next(err)
+    }
+}
--- src/routes/oneinch.ts
+++ src/routes/oneinch.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 
-import { getCurrentValue, getProfitAndLoss } from '../controllers/oneinch'
+import {
+    getCurrentValue,
+    getProfitAndLoss,
+    getTokenDetails
+} from '../controllers/oneinch'
 
 const router = Router()
 
 router.post('/get-current-value', getCurrentValue)
 router.post('/get-profit-and-loss', getProfitAndLoss)
+router.post('/get-token-details', getTokenDetails)
 
 export default router
