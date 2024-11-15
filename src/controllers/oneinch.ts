a3e739bb4ce2ebdce27473aabc0900e8c40cfb49
Mirko Basic <bejzik8@gmail.com>
2024-11-16 04:03:26 +0700

    Created profit and loss route.

--- src/controllers/oneinch.ts
+++ src/controllers/oneinch.ts
@@ -19,3 +19,22 @@ export const getCurrentValue: RequestHandler = async (req, res, next) => {
         next(err)
     }
 }
+
+export const getProfitAndLoss: RequestHandler = async (req, res, next) => {
+    const { walletAddress, chainId, fromTimestamp, toTimestamp } = req.body
+
+    try {
+        const { data } = await axios.get(
+            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss?addresses=${walletAddress}&chain_id=${chainId}&from_timestamp=${fromTimestamp}&to_timestamp=${toTimestamp}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+                }
+            }
+        )
+
+        res.status(200).json({ response: data })
+    } catch (err) {
+        next(err)
+    }
+}
--- src/routes/oneinch.ts
+++ src/routes/oneinch.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 
-import { getCurrentValue } from '../controllers/oneinch'
+import { getCurrentValue, getProfitAndLoss } from '../controllers/oneinch'
 
 const router = Router()
 
 router.post('/get-current-value', getCurrentValue)
+router.post('/get-profit-and-loss', getProfitAndLoss)
 
 export default router
