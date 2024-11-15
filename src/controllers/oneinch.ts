0efdfd109598ad9988eccd129dba1ff7d2707979
Mirko Basic <bejzik8@gmail.com>
2024-11-16 04:13:35 +0700

    Updated profit and loss route.

--- src/controllers/oneinch.ts
+++ src/controllers/oneinch.ts
@@ -21,11 +21,11 @@ export const getCurrentValue: RequestHandler = async (req, res, next) => {
 }
 
 export const getProfitAndLoss: RequestHandler = async (req, res, next) => {
-    const { walletAddress, chainId, fromTimestamp, toTimestamp } = req.body
+    const { walletAddress, chainId } = req.body
 
     try {
         const { data } = await axios.get(
-            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss?addresses=${walletAddress}&chain_id=${chainId}&from_timestamp=${fromTimestamp}&to_timestamp=${toTimestamp}`,
+            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss?addresses=${walletAddress}&chain_id=${chainId}&timerange=1day`,
             {
                 headers: {
                     Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
