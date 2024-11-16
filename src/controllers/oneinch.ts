8aa8f8d5e84e4c4efde69659935474f2a26825e6
Mirko Basic <bejzik8@gmail.com>
2024-11-16 23:13:26 +0700

    Updated fetching functions.

--- src/controllers/oneinch.ts
+++ src/controllers/oneinch.ts
@@ -2,14 +2,21 @@ import { RequestHandler } from 'express'
 import axios from 'axios'
 
 export const getCurrentValue: RequestHandler = async (req, res, next) => {
-    const { walletAddress, chainId } = req.body
+    const { chain_id, addresses } = req.body
 
     try {
         const { data } = await axios.get(
-            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/current_value?addresses=${walletAddress}&chain_id=${chainId}`,
+            'https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/current_value',
             {
+                params: {
+                    chain_id,
+                    addresses
+                },
                 headers: {
                     Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+                },
+                paramsSerializer: {
+                    indexes: null
                 }
             }
         )
@@ -21,14 +28,24 @@ export const getCurrentValue: RequestHandler = async (req, res, next) => {
 }
 
 export const getProfitAndLoss: RequestHandler = async (req, res, next) => {
-    const { walletAddress, chainId } = req.body
+    const { chain_id, addresses } = req.body
+
+    console.log('A', addresses, chain_id)
 
     try {
         const { data } = await axios.get(
-            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss?addresses=${walletAddress}&chain_id=${chainId}&timerange=1day`,
+            'https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss',
             {
+                params: {
+                    chain_id,
+                    addresses,
+                    timerange: '1day'
+                },
                 headers: {
                     Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+                },
+                paramsSerializer: {
+                    indexes: null
                 }
             }
         )
@@ -40,19 +57,42 @@ export const getProfitAndLoss: RequestHandler = async (req, res, next) => {
 }
 
 export const getTokenDetails: RequestHandler = async (req, res, next) => {
-    const { walletAddress, chainId } = req.body
+    const { chain_id, addresses } = req.body
 
     try {
         const { data } = await axios.get(
-            `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/details?addresses=${walletAddress}&chain_id=${chainId}&timerange=1day`,
+            'https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/details',
             {
+                params: {
+                    chain_id,
+                    addresses,
+                    timerange: '1day'
+                },
                 headers: {
                     Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+                },
+                paramsSerializer: {
+                    indexes: null
                 }
             }
         )
 
-        console.log(data)
+        res.status(200).json({ response: data })
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getTokens: RequestHandler = async (_, res, next) => {
+    try {
+        console.log('GEtting...')
+        const { data } = await axios.get('https://api.1inch.dev/token/v1.2/1', {
+            headers: {
+                Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+            }
+        })
+
+        console.log('data', data)
 
         res.status(200).json({ response: data })
     } catch (err) {
--- src/routes/oneinch.ts
+++ src/routes/oneinch.ts
@@ -3,7 +3,8 @@ import { Router } from 'express'
 import {
     getCurrentValue,
     getProfitAndLoss,
-    getTokenDetails
+    getTokenDetails,
+    getTokens
 } from '../controllers/oneinch'
 
 const router = Router()
@@ -11,5 +12,7 @@ const router = Router()
 router.post('/get-current-value', getCurrentValue)
 router.post('/get-profit-and-loss', getProfitAndLoss)
 router.post('/get-token-details', getTokenDetails)
+
+router.get('/get-tokens', getTokens)
 
 export default router
